49973e4fb843d3d684cb35ef0f23cc17a1670466
Rhogoro Norine <rhogoron@un-k6.kzn.21-school.ru>
2024-06-13 14:08:06 +0300

    eureka

--- EurekaServer/src/main/java/org/example/AccessControlServiceApplication.java
+++ EurekaServer/src/main/java/org/example/AccessControlServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class AccessControlServiceApplication {
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(AccessControlServiceApplication.class);
+        SpringApplication.run(AccessControlServiceApplication.class, args);
     }
 }
+
