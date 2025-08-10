72711ae66352bd2843a1e587ad6f826c3447d2d9
Alana Enikeeva <alanaenikeeva@mail.ru>
2025-08-10 20:34:52 +0300

    1

--- EurekaServer/src/main/java/org/example/AccessControlServiceApplication.java
+++ EurekaServer/src/main/java/org/example/AccessControlServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// 11
 @SpringBootApplication
 @EnableEurekaServer
 public class AccessControlServiceApplication {
